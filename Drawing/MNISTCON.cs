3f008f003ee64807dbb9dd282544bfbcb4448909
Tom-Bumhead <thomas.james.marsden@gmail.com>
2022-03-11 12:12:06 +0000

    added comments to MNISTCON

--- Drawing/MNISTCON.cs
+++ Drawing/MNISTCON.cs
@@ -9,28 +9,30 @@ namespace NeuralNetworkLearning
         public List<DigitImage> images = new List<DigitImage>();
         public MNISTCON(string ToT)
         {
+            //creates the stream to train images and labels files
             FileStream ifsLabels = ToT == "Train" ? new FileStream("train-labels.idx1-ubyte", FileMode.Open) : new FileStream("t10k-labels.idx1-ubyte", FileMode.Open);
             FileStream ifsImages = ToT == "Train" ? new FileStream("train-images.idx3-ubyte", FileMode.Open) : new FileStream("t10k-images.idx3-ubyte", FileMode.Open);
             int iterations = ToT == "Train" ? 60000 : 10000;
 
-
+            //creates binary readers for each file
             BinaryReader brLabels = new BinaryReader(ifsLabels);
             BinaryReader brImages = new BinaryReader(ifsImages);
 
+            //meta data
             int magic1 = brImages.ReadInt32();
             int numImages = brImages.ReadInt32();
             int numRows = brImages.ReadInt32();
             int numCols = brImages.ReadInt32();
 
-
             int magic2 = brLabels.ReadInt32();
             int numLabels = brLabels.ReadInt32();
 
+            //creates 2D pixel array
             byte[][] pixels = new byte[28][];
             for (int i = 0; i < pixels.Length; ++i)
                 pixels[i] = new byte[28];
 
-            // each test image
+            //reads each image as greyscale image
             for (int di = 0; di < iterations; ++di)
             {
                 for (int i = 0; i < 28; ++i)
@@ -41,14 +43,18 @@ namespace NeuralNetworkLearning
                         pixels[i][j] = b;
                     }
                 }
-
+                //reads label
                 byte lbl = brLabels.ReadByte();
 
+                //creates DigitImage from label and pixel data
                 DigitImage dImage =
                   new DigitImage(pixels, lbl);
-                images.Add(dImage);
-            } // each image
 
+                //adds to list of DigitImage
+                images.Add(dImage);
+            }
+
+            //closes all streams
             ifsImages.Close();
             brImages.Close();
             ifsLabels.Close();
@@ -65,6 +71,7 @@ namespace NeuralNetworkLearning
         public DigitImage(byte[][] pixels,
           byte label)
         {
+            //initialises pixels and sets pixel data
             this.pixels = new byte[28][];
             for (int i = 0; i < this.pixels.Length; ++i)
                 this.pixels[i] = new byte[28];
@@ -73,11 +80,13 @@ namespace NeuralNetworkLearning
                 for (int j = 0; j < 28; ++j)
                     this.pixels[i][j] = pixels[i][j];
 
+            //sets label
             this.label = label;
         }
 
         public override string ToString()
         {
+            //creates ASCII representation of image
             string s = "";
             for (int i = 0; i < 28; ++i)
             {
@@ -92,9 +101,10 @@ namespace NeuralNetworkLearning
                 }
                 s += "\n";
             }
-            s += this.label.ToString();
+            //adds label to end of string
+            s += label.ToString();
             return s;
-        } // ToString
+        }
 
     }
 
